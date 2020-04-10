3da6f4fdf7d3c31d34e67f7a0095dd3444248c00
saxymatt <63033522+saxymatt@users.noreply.github.com>
2020-04-09 23:02:52 -0400

    Update portScannerThreading.py

--- portScannerThreading.py
+++ portScannerThreading.py
@@ -36,15 +36,3 @@ for worker in range(1025):
 q.join()
 
 print('The port scan took: ',time.time()-start_time)
-
-#If you want to prompt the user for port numbers
-#while True:
-#       try:
-#           ports = input('Enter number of ports to scan: ')
-#           if int(ports) <= 0 or int(ports) > 65535:
-#               raise ValueError
-#           break
-#       except ValueError:
-#          print("Invalid port number. Must be in the range of 1-65535.")
-#
-#print('Scanning ports 1-',ports,sep='')
